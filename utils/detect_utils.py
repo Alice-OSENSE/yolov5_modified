ef929bc3e5a150010295c68151c658002b608e47
Alice-OSENSE <alice.li@osensetech.com>
2020-11-04 15:39:02 +0800

    Start refactoring 'detect.py'

--- utils/detect_utils.py
+++ utils/detect_utils.py
@@ -1,4 +1,13 @@
 import numpy as np
 
 def prepare_for_density_map(img):
-    pass
+    dmap = np.zeros([img.shape[0], img.shape[1], 1])
+    x_axis = np.linspace(0, img.shape[0], img.shape[0])
+    y_axis = np.linspace(0, img.shape[1], img.shape[1])
+    x, y = np.meshgrid(x_axis, y_axis)
+
+    # Pack x and y into a single 3-dimensional array
+    pos = np.empty(x.shape + (2,))
+    pos[:, :, 0] = x
+    pos[:, :, 1] = y
+    return dmap, pos, None, None
